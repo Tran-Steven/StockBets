aa9de6443ed5dc360157666412d8bf89b648ea7d
owen <osn53387@uga.edu>
2023-02-07 12:01:53 -0500

    Implemented some of header and footer comps.

---
+++ pages/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from '@styles/Footer.module.css'
+import Card from '../Cards/Card'
+
+export default function Footer() {
+    return (
+        <div className={styles.footer}>
+            <h3>Collaberators</h3> 
+        </div>
+    )
+}
