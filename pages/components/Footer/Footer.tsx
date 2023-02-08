0c2f3c7f23ee159bb5de07354db393447c058b81
owen <osn53387@uga.edu>
2023-02-08 14:07:55 -0500

    Updated card info of collaberators

--- pages/components/Footer/Footer.tsx
+++ pages/components/Footer/Footer.tsx
@@ -6,9 +6,9 @@ export default function Footer() {
         <div className={styles.footer}>
             <h3>Collaberators</h3> 
             <div className={styles.cardBox}>
-            <Card Name={"Owen Na"} email={"owensungminna@gmail.com"} linkdinLink={""} githubLink={""}/>
+            <Card Name={"Owen Na"} email={"owensungminna@gmail.com"} linkdinLink={"https://www.linkedin.com/in/owen-na-634247236/"} githubLink={"https://github.com/owen-na"}/>
             <Card Name={"Steven Tran"} email={"stevenntran@outlook.com"} linkdinLink={"https://www.linkedin.com/in/steven-tran-26735b206"} githubLink={"https://www.github.com/Tran-Steven/"}/>
-            <Card Name={"Owen Na"} email={"owensungminna@gmail.com"} linkdinLink={""} githubLink={""}/>
+            <Card Name={"Owen Na"} email={"zhizhengwang28@gmail.com"} linkdinLink={"www.linkedin.com/in/zhizheng-wang28"} githubLink={"https://github.com/Zhizheng28"}/>
             </div>
         </div>
     )
