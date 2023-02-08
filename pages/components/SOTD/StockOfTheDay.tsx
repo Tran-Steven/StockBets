e54f6c170f40364063dc21d886d2dc870633f3ad
owen <osn53387@uga.edu>
2023-02-08 16:22:07 -0500

    Begin implementing main content of SOTD

---
+++ pages/components/SOTD/StockOfTheDay.tsx
@@ -0,0 +1,13 @@
+import styles from '@styles/StockofTheDay.module.css'
+
+export default function StockofTheDay() {
+    return (
+        <div className={styles.mainContent}>
+            <div className={styles.loadingBar}>
+
+            </div>
+        </div>
+    )
+
+
+}
