042a399a0b5266202c345d844322a64323230fc5
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-02-09 10:20:20 -0500

    Fixed some typos

--- pages/components/Cards/Card.tsx
+++ pages/components/Cards/Card.tsx
@@ -1,26 +1,26 @@
-import style from '@styles/Card.module.css'
-import Link from 'next/link'
+import style from "@styles/Card.module.css";
+import Link from "next/link";
 
 export interface CardInfo {
-    Name: string;
-    email: string;
-    linkdinLink: string;
-    githubLink: string;
+  Name: string;
+  email: string;
+  linkedinLink: string;
+  githubLink: string;
 }
 
 export default function Card(prop: CardInfo) {
-    return (
-        <div className={style.card}>
-            <p>{prop.Name}</p>
-            <p>{prop.email}</p>
-            <div className={style.cardBottom}>
-            <Link href="/about" legacyBehavior>
-                <p>Linkdin</p>
-            </Link>
-            <Link href="/about" legacyBehavior>
-                <p>Github</p>
-            </Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className={style.card}>
+      <p>{prop.Name}</p>
+      <p>{prop.email}</p>
+      <div className={style.cardBottom}>
+        <Link href="/about" legacyBehavior>
+          <p>LinkedIn</p>
+        </Link>
+        <Link href="/about" legacyBehavior>
+          <p>Github</p>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- pages/components/Footer/Footer.tsx
+++ pages/components/Footer/Footer.tsx
@@ -1,15 +1,30 @@
-import styles from '@styles/Footer.module.css'
-import Card from '../Cards/Card'
+import styles from "@styles/Footer.module.css";
+import Card from "../Cards/Card";
 
 export default function Footer() {
-    return (
-        <div className={styles.footer}>
-            <h3>Collaberators</h3> 
-            <div className={styles.cardBox}>
-            <Card Name={"Owen Na"} email={"owensungminna@gmail.com"} linkdinLink={"https://www.linkedin.com/in/owen-na-634247236/"} githubLink={"https://github.com/owen-na"}/>
-            <Card Name={"Steven Tran"} email={"stevenntran@outlook.com"} linkdinLink={"https://www.linkedin.com/in/steven-tran-26735b206"} githubLink={"https://www.github.com/Tran-Steven/"}/>
-            <Card Name={"Owen Na"} email={"zhizhengwang28@gmail.com"} linkdinLink={"www.linkedin.com/in/zhizheng-wang28"} githubLink={"https://github.com/Zhizheng28"}/>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.footer}>
+      <h3>Collaborators</h3>
+      <div className={styles.cardBox}>
+        <Card
+          Name={"Owen Na"}
+          email={"owensungminna@gmail.com"}
+          linkedinLink={"https://www.linkedin.com/in/owen-na-634247236/"}
+          githubLink={"https://github.com/owen-na"}
+        />
+        <Card
+          Name={"Steven Tran"}
+          email={"stevenntran@outlook.com"}
+          linkedinLink={"https://www.linkedin.com/in/steven-tran-26735b206"}
+          githubLink={"https://www.github.com/Tran-Steven/"}
+        />
+        <Card
+          Name={"Zhizheng Wang"}
+          email={"zhizhengwang28@gmail.com"}
+          linkedinLink={"www.linkedin.com/in/zhizheng-wang28"}
+          githubLink={"https://github.com/Zhizheng28"}
+        />
+      </div>
+    </div>
+  );
 }
