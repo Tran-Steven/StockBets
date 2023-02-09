37f1f6a40fabe29354eed573673ee49e6c1f9485
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-02-09 10:45:30 -0500

    Changed font of body & fontweight of header

--- pages/components/Header/Header.tsx
+++ pages/components/Header/Header.tsx
@@ -6,8 +6,8 @@ export default function Header() {
       {/* <image src="" alt="StockBets-logo" className="logo"/> */}
       <div className={styles.headerRightSide}>
         <a>SOTD</a>
-        <a>Account Stats</a>
-        <a>Leagues</a>
+        <a>ACCOUNT</a>
+        <a>LEAGUES</a>
         <div className="navlogin">
           <button className={styles.headerButton}>Register</button>
           <button className={styles.headerButton}>Login</button>
