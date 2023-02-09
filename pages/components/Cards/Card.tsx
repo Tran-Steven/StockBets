7064549960b141e272dd27f4b0874923243cbf86
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-02-09 14:19:52 -0500

    changed footer collab layout

--- pages/components/Cards/Card.tsx
+++ pages/components/Cards/Card.tsx
@@ -2,7 +2,7 @@ import style from "@styles/Card.module.css";
 import Link from "next/link";
 import github from "@assets/github.svg";
 import linkedin from "@assets/linkedin.svg";
-
+import email from "@assets/email.svg";
 import Image from "next/image";
 export interface CardInfo {
   Name: string;
@@ -15,14 +15,26 @@ export default function Card(prop: CardInfo) {
   return (
     <div className={style.card}>
       <p>{prop.Name}</p>
-      <p>{prop.email}</p>
       <div className={style.cardBottom}>
         <a href={prop.linkedinLink} target="_blank" rel="noreferrer">
-          <Image src={linkedin} alt="LinkedIn" loading="lazy" />
+          <Image
+            src={linkedin}
+            alt="LinkedIn"
+            loading="lazy"
+            className={style.svg}
+          />
+        </a>
+        <a href={prop.githubLink} target="_blank" rel="noreferrer">
+          <Image
+            src={github}
+            className={style.svg}
+            alt="GitHub"
+            loading="lazy"
+          />
         </a>
 
-        <a href={prop.githubLink} target="_blank" rel="noreferrer">
-          <Image src={github} alt="GitHub" loading="lazy" />
+        <a href={`mailto:` + prop.email} target="_blank" rel="noreferrer">
+          <Image src={email} className={style.svg} alt="Email" loading="lazy" />
         </a>
       </div>
     </div>
