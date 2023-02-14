2b30d920c9c1e3a89e1e4243a8e5ba1df9d3b2da
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-02-14 13:59:26 -0500

    Made header more responsive

--- pages/components/Header/Header.tsx
+++ pages/components/Header/Header.tsx
@@ -5,13 +5,16 @@ export default function Header() {
     /**Add media query to create hamburger menu starting at approx 1284width */
     <header className={styles.header}>
       {/* <image src="" alt="StockBets-logo" className="logo"/> */}
+      <div className={styles.headerLeftSide}>
+        <h2>Placeholder</h2>
+      </div>
       <div className={styles.headerRightSide}>
         <a>SOTD</a>
         <a>ACCOUNT</a>
         <a>LEAGUES</a>
         <div className="navlogin">
-          <button className={styles.headerButton}>Register</button>
-          <button className={styles.headerButton}>Login</button>
+          <button className={styles.headerButton}>REGISTER</button>
+          <button className={styles.headerButton}>LOGIN</button>
         </div>
       </div>
     </header>
