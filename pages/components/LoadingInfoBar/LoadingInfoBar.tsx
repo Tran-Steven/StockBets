1eb1ff7ee4247b244520f9473a8bf1ec81ac234f
owen <osn53387@uga.edu>
2023-02-14 14:34:10 -0500

    ARC FINALLY DISPLAYS ITSELF

--- pages/components/LoadingInfoBar/LoadingInfoBar.tsx
+++ pages/components/LoadingInfoBar/LoadingInfoBar.tsx
@@ -3,9 +3,12 @@ import styles from '@styles/LoadingInfobar.module.css'
 export default function LoadingInfobar() {
     return (
         <div className={styles.loadingBar}>
-            <div className={styles.innerBarContent}>
+            <svg width="600" height="600">
+                <path d="M 150 287 A 100 100 120 1 1 250 287" />
+            </svg>    
+            {/* <div className={styles.innerBarContent}>
 
-            </div>
+            </div> */}
         </div>
     )
 }
