5c689bdde66f8757c99693ec89abbe339285e90e
owen <osn53387@uga.edu>
2023-02-21 01:51:54 -0500

    updated loadingbar postioning

--- pages/components/LoadingInfoBar/LoadingInfoBar.tsx
+++ pages/components/LoadingInfoBar/LoadingInfoBar.tsx
@@ -3,12 +3,10 @@ import styles from '@styles/LoadingInfobar.module.css'
 export default function LoadingInfobar() {
     return (
         <div className={styles.loadingBar}>
-            <svg width="600" height="600">
-                <path d="M 150 287 A 100 100 120 1 1 250 287" />
+            <svg>
+                <path d="M 150 287 A 200 200 120 1 1 250 287" />
             </svg>    
-            {/* <div className={styles.innerBarContent}>
-
-            </div> */}
+            <div className={styles.innerBarContent}></div>
         </div>
     )
 }
