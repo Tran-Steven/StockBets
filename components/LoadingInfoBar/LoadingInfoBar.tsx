c72a02a62c543be5dfafa491343f39bfe10e87a7
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-02-21 10:44:58 -0500

    Moved

--- components/LoadingInfoBar/LoadingInfoBar.tsx
+++ components/LoadingInfoBar/LoadingInfoBar.tsx
@@ -1,14 +1,12 @@
-import styles from '@styles/LoadingInfobar.module.css'
+import styles from "@styles/LoadingInfobar.module.css";
 
 export default function LoadingInfobar() {
-    return (
-        <div className={styles.loadingBar}>
-            <svg>
-            <path d=" M 465 430 A 200 200 120 1 1 665 430" />
-            </svg>    
-            <div className={styles.innerBarContent}>
-
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.loadingBar}>
+      <svg>
+        <path d="M 150 287 A 200 200 120 1 1 250 287" />
+      </svg>
+      <div className={styles.innerBarContent}></div>
+    </div>
+  );
 }
