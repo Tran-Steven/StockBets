d2c0851536b89bbce49f5a1f1ae74ac680779bca
Owen Na <102653237+owen-na@users.noreply.github.com>
2023-02-21 10:46:54 -0500

    Changes to svg (#9)
    * Made header more responsive
    
    * fix darkmode
    
    * Further progress on positioning of loading bar and info
    
    * Moved home,globals style inside styles folder
    
    * Centered some main content
    
    * Made header more responsive
    
    * ARC FINALLY DISPLAYS ITSELF
    
    * fix darkmode
    
    * updated loadingbar postioning
    
    * Further progress on positioning of loading bar and info
    
    * Restructured the locations of the components
    
    * Moved
    
    ---------
    
    Co-authored-by: Steven Tran <steventran@Stevens-MacBook-Pro.local>
    Co-authored-by: Owen Na <newseoul1302@gmail.com>

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,24 @@
 import styles from "@styles/Header.module.css";
+import Link from "next/link";
 
 export default function Header() {
   return (
     /**Add media query to create hamburger menu starting at approx 1284width */
     <header className={styles.header}>
       {/* <image src="" alt="StockBets-logo" className="logo"/> */}
+      <div className={styles.headerLeftSide}>
+        <h2>Placeholder</h2>
+      </div>
       <div className={styles.headerRightSide}>
-        <a>SOTD</a>
-        <a>ACCOUNT</a>
-        <a>LEAGUES</a>
+        <Link href="/">SOTD</Link>
+
+        <Link href="/account">ACCOUNT</Link>
+
+        <Link href="/leagues">LEAGUES</Link>
+
         <div className="navlogin">
-          <button className={styles.headerButton}>Register</button>
-          <button className={styles.headerButton}>Login</button>
+          <button className={styles.headerButton}>REGISTER</button>
+          <button className={styles.headerButton}>LOGIN</button>
         </div>
       </div>
     </header>
--- pages/account.tsx
+++ pages/account.tsx
@@ -1,18 +1,15 @@
-import Head from "next/head";
+import APC from "@components/AccountPageContent/AccountContent";
 import NavBar from "@components/Header/Header";
 import Footer from "@components/Footer/Footer";
 import Image from "next/image";
-import SOTD from "@components/SOTD/StockOfTheDay";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+import styles from "@styles/Home.module.css";
 
-// const inter = Inter({ subsets: ["Open Sans"] style: []})
-
-export default function Home() {
+export default function Account() {
   return (
     <>
       <NavBar />
-      <SOTD />
+      <APC />
       <Footer />
     </>
   );
