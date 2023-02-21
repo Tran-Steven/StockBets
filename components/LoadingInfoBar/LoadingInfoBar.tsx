452597b442c0aaebd06ce8bcb06d7d6643407357
owen <osn53387@uga.edu>
2023-02-21 15:02:40 -0500

    Fixed postioning of footer and SOTD divs

--- components/LoadingInfoBar/LoadingInfoBar.tsx
+++ components/LoadingInfoBar/LoadingInfoBar.tsx
@@ -3,7 +3,6 @@ import styles from "@styles/LoadingInfobar.module.css";
 export default function LoadingInfobar() {
   return (
     <div className={styles.loadingBar}>
-
         <svg viewBox="0 0 500 500">
           <path d=" M 250 400 A 200 200 120 1 1 450 400" />
         </svg>    
