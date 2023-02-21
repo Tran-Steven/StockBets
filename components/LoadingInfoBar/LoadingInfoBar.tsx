2b5a30f8d1a55dda7b56403681e208b5abd6e332
Owen Na <102653237+owen-na@users.noreply.github.com>
2023-02-21 11:38:33 -0500

    Owen na branch frontend (#10)
    * Changes to svg (#9)
    
    * Made header more responsive
    
    * fix darkmode
    
    * Further progress on positioning of loading bar and info
    
    * Moved home,globals style inside styles folder
    
    * Centered some main content
    
    * Made header more responsive
    
    * ARC FINALLY DISPLAYS ITSELF
    
    * fix darkmode
    
    * updated loadingbar postioning
    
    * Further progress on positioning of loading bar and info
    
    * Restructured the locations of the components
    
    * Moved
    
    ---------
    
    Co-authored-by: Steven Tran <steventran@Stevens-MacBook-Pro.local>
    Co-authored-by: Owen Na <newseoul1302@gmail.com>
    
    * SVG fix
    
    ---------
    
    Co-authored-by: Steven Tran <steventran@Stevens-MacBook-Pro.local>
    Co-authored-by: Owen Na <newseoul1302@gmail.com>

--- components/LoadingInfoBar/LoadingInfoBar.tsx
+++ components/LoadingInfoBar/LoadingInfoBar.tsx
@@ -3,10 +3,13 @@ import styles from "@styles/LoadingInfobar.module.css";
 export default function LoadingInfobar() {
   return (
     <div className={styles.loadingBar}>
-      <svg>
-        <path d="M 150 287 A 200 200 120 1 1 250 287" />
-      </svg>
-      <div className={styles.innerBarContent}></div>
+
+        <svg viewBox="0 0 500 500">
+          <path d=" M 250 400 A 200 200 120 1 1 450 400" />
+        </svg>    
+      <div className={styles.innerBarContent}>
+     </div>
+
     </div>
   );
 }
