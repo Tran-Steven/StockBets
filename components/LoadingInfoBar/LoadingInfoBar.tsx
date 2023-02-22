72cadcf1644fc642dec038598b3cf0620c0c178c
owen <osn53387@uga.edu>
2023-02-22 15:35:24 -0500

    FIXED POSTIONING OF MAINCONTENT

--- components/LoadingInfoBar/LoadingInfoBar.tsx
+++ components/LoadingInfoBar/LoadingInfoBar.tsx
@@ -4,7 +4,8 @@ export default function LoadingInfobar() {
   return (
     <div className={styles.loadingBar}>
         <svg viewBox="0 0 500 500">
-          <path d=" M 250 400 A 200 200 120 1 1 450 400" />
+          <circle cx="250" cy="250" r="250" pathLength="100" strokeDasharray="75 25"/>
+          {/* // <path d=" M 250 400 A 200 200 120 1 1 450 400" /> */}
         </svg>    
       <div className={styles.innerBarContent}>
      </div>
