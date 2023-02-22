d9dc62a8de420b1290064e83971f5344bb883d6a
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-02-22 16:26:07 -0500

    fixed some routing

---
+++ components/LeaguePageContent/LeaguePageContent.tsx
@@ -0,0 +1,7 @@
+export default function LeaguePageContent() {
+  return (
+    <div>
+      <h2>LP Content</h2>
+    </div>
+  );
+}
--- pages/leagues.tsx
+++ pages/leagues.tsx
@@ -1,15 +1,15 @@
-import APC from "@components/AccountPageContent/AccountContent";
+import LPC from "@components/LeaguePageContent/LeaguePageContent";
 import NavBar from "@components/Header/Header";
 import Footer from "@components/Footer/Footer";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@styles/Home.module.css";
 
-export default function account() {
+export default function leagues() {
   return (
     <>
       <NavBar />
-      <APC />
+      <LPC />
       <Footer />
     </>
   );
