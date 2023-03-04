bcb5e772d3a45ef767622a09e734270aa515c690
owen <osn53387@uga.edu>
2023-03-04 15:35:42 -0500

    Loading Bar fills statically

--- components/LoadingInfoBar/LoadingInfoBar.tsx
+++ components/LoadingInfoBar/LoadingInfoBar.tsx
@@ -6,13 +6,35 @@ export interface StockInfo {
   CurrentPrice: number;
 }
 
-const Progress = ({percent = 40,}) => {
+function calculatePercent() {
+  let percentFill = 0;
+  let countDownDate = new Date("Mar 4, 2024 16:00:00").getTime();
+    const x = setInterval(function() {
+
+  // Get today's date and time
+  const now = new Date().getTime();
+  
+
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  }, 1000);
+
+
+}
+
+const Progress = ({percent = 50}) => {
 const arc = 100;
 const percentNormalized = Math.min(Math.max(percent, 0), 100);
 const offset = arc - (percentNormalized / 100) * arc;
   return (
     <svg viewBox="0 0 500 500">
-      <circle className={styles.progress} cx="250" cy="250" r="250" pathLength="100" strokeDasharray={`${arc} ${arc - offset}`}/>
+      <circle className={styles.progress} cx="250" cy="250" r="250" pathLength="100" strokeDasharray={`${offset} ${arc}`} strokeLinecap="round"/>
     </svg>
   )
 }
@@ -21,7 +43,7 @@ export default function LoadingInfobar(prop: StockInfo) {
   return (
     <div className={styles.loadingBar}>
         <svg viewBox="0 0 500 500">
-          <circle cx="250" cy="250" r="250" pathLength="100" strokeDasharray="75 25"/>
+          <circle cx="250" cy="250" r="250" pathLength="100" strokeDasharray="75 25" strokeLinecap="round"/>
           <Progress />
           {/* // <path d=" M 250 400 A 200 200 120 1 1 450 400" /> */}
         </svg>    
