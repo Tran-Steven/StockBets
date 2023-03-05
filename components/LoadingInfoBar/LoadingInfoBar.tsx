4747034ac8c62aec1fa669b7de7fbefa782bafed
owen <osn53387@uga.edu>
2023-03-05 16:45:20 -0500

    Rough static frontend implemented

--- components/LoadingInfoBar/LoadingInfoBar.tsx
+++ components/LoadingInfoBar/LoadingInfoBar.tsx
@@ -11,8 +11,17 @@ let counter = 0;
 function calculatePercent() {
 
   let percentFill = 100;
-  let dateSet = new Date("Mar 4, 2024 16:00:00");
+  let dateSet = new Date(); // "Mar 4, 2024 16:00:00"
+  const day = dateSet.getDay();
+  const startHours = dateSet.setHours(9, 0,0);
+  const endHours = dateSet.setHours(4, 0 ,0)
   let countDownDate = new Date(dateSet.getDate()).getTime();
+
+  if (countDownDate < startHours || countDownDate > endHours) { // accounts for closed hours
+    return percentFill = 25;
+  } else if (day == 0 || day == 6) { // acounts for weekends
+    return percentFill = 25;
+  }
 
   while(percentFill != 25) {
   const x = setInterval(function() {
@@ -45,8 +54,7 @@ function calculatePercent() {
 }
 
 
-const Progress = ({percent = calculatePercent()}) => {
-
+const Progress = ({percent = calculatePercent()}) => { // implementation of how much of progresssvg covers the originial svg
 const arc = 100;
 const percentNormalized = Math.min(Math.max(percent, 0), 100);
 const offset = arc - (percentNormalized / 100) * arc;
@@ -63,14 +71,13 @@ export default function LoadingInfobar(prop: StockInfo) {
         <svg viewBox="0 0 500 500">
           <circle cx="250" cy="250" r="250" pathLength="100" strokeDasharray="75 25" strokeLinecap="round"/>
           <Progress />
-          {/* // <path d=" M 250 400 A 200 200 120 1 1 450 400" /> */}
         </svg>    
       <div className={styles.innerBarContent}>
 
 
-          <h2>{prop.StockName} AAPL</h2>
-          <p>Current Price: 145{prop.CurrentPrice}</p>
-          <p>Starting Price: 120{prop.StartingPrice}</p>
+          <h2>{prop.StockName}</h2>
+          <p>Current Price: {prop.CurrentPrice}</p>
+          <p>Starting Price: {prop.StartingPrice}</p>
 
      </div>
 
