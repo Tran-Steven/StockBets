3171e440db972be1e062efcbba0af7902e2e76e4
Owen Na <102653237+owen-na@users.noreply.github.com>
2023-03-07 16:56:25 -0500

    Owen na branch frontend (#23)
    * Changes to svg (#9)
    
    * Made header more responsive
    
    * fix darkmode
    
    * Further progress on positioning of loading bar and info
    
    * Moved home,globals style inside styles folder
    
    * Centered some main content
    
    * Made header more responsive
    
    * ARC FINALLY DISPLAYS ITSELF
    
    * fix darkmode
    
    * updated loadingbar postioning
    
    * Further progress on positioning of loading bar and info
    
    * Restructured the locations of the components
    
    * Moved
    
    ---------
    
    Co-authored-by: Steven Tran <steventran@Stevens-MacBook-Pro.local>
    Co-authored-by: Owen Na <newseoul1302@gmail.com>
    
    * SVG fix
    
    * Testing merge issue
    
    * Fixed postioning of footer and SOTD divs
    
    * Loading Bar stays visible at all ratios
    
    * FIXED POSTIONING OF MAINCONTENT
    
    * Removed some uneeded lines
    
    * Added Props for SOTD, cleaned up innerContent
    
    * In Progress on real time loadingbar
    
    * Loading Bar fills statically
    
    * Figured out total minutes till next day
    
    * Rough static frontend implemented
    
    * calculating time difference for percent fill now works as intended
    
    * Removed some unneeded code
    
    * Changed some styling for mainpage
    
    * Changed some styling, began working on Acc info page
    
    ---------
    
    Co-authored-by: Steven Tran <steventran@Stevens-MacBook-Pro.local>
    Co-authored-by: Owen Na <newseoul1302@gmail.com>

--- components/AccountPageContent/AccountContent.tsx
+++ components/AccountPageContent/AccountContent.tsx
@@ -1,7 +1,48 @@
+import style from "@styles/AccountContent.module.css"
+
 export default function AccountContent() {
   return (
-    <div>
-      <h2>AccountContent</h2>
+    <div className={style.placeHolder}>
+    <h2>Account Info</h2>
+    <div className={style.parent}>
+      <div className={style.mainContent}>
+          <ul>
+            <img src="" alt="Profile Picture"></img>
+            <li>Total Guesses</li>
+            <li>Guesses Correct (%) </li>
+          </ul>
+      </div>
+    </div>
     </div>
   );
 }
+
+// bottom code maybe used for a pfp functionality
+// will also need to figure out what the hell to do with this accound info page
+
+// import Image from 'next/image';
+
+// const ProfilePic = () => {
+//   const [imgSrc, setImgSrc] = useState('/placeholder.jpg');
+
+//   const handleFileChange = (e) => {
+//     const file = e.target.files[0];
+//     if (file) {
+//       const reader = new FileReader();
+//       reader.addEventListener('load', () => {
+//         setImgSrc(reader.result);
+//       });
+//       reader.readAsDataURL(file);
+//     }
+//   };
+
+//   return (
+//     <div className="profile-pic">
+//       <Image src={imgSrc} alt="Profile Picture" width={100} height={100} className="rounded-full" />
+//       <label htmlFor="profile-pic-input">
+//         <i className="fa fa-camera"></i>
+//       </label>
+//       <input type="file" id="profile-pic-input" accept="image/*" onChange={handleFileChange} />
+//     </div>
+//   );
+// };
--- pages/account.tsx
+++ pages/account.tsx
@@ -10,7 +10,7 @@ export default function Account() {
     <>
       <NavBar />
       <APC />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
