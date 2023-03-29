1d6dc590f6ed1f53c8897c5d497620e07fad1f92
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-03-29 16:25:40 -0400

    header fix ish and api call

--- components/SOTD/StockOfTheDay.tsx
+++ components/SOTD/StockOfTheDay.tsx
@@ -1,19 +1,60 @@
 import styles from "@styles/StockofTheDay.module.css";
 import LoadingInfobar from "../LoadingInfoBar/LoadingInfoBar";
 import LeaderBoard from "@components/LeaderBoard/LeaderBoard";
-// const worker = new Worker("worker.tsx");
+import f500ticker from "@assets/f500tickers.json";
+import axios from "axios";
+import Random from "@assets/Random";
+import React, { useState, useEffect } from "react";
 
-// function isMarketOpen() {
-//   worker.postMessage("Status");
-// }
-// isMarketOpen();
 export default function StockofTheDay() {
+  let date = new Date();
+  let marketStatus = false;
+  let StockData;
+  let stockRefreshDate;
+  const tickerJSON = f500ticker;
+  const [ticker, setTicker] = useState("");
+
+  function check() {
+    if (date.getUTCHours() >= 14 && date.getUTCMinutes() >= 30) {
+      if (date.getUTCHours() <= 21) {
+        return (marketStatus = true);
+      }
+    }
+    return (marketStatus = true);
+  }
+
+  const setRandTicker = function () {
+    setTicker(tickerJSON[Random()].Symbol.valueOf());
+  };
+
+  async function getStockData() {
+    if (marketStatus == true) {
+      if (ticker == "" || date.getUTCHours() == 14) {
+        setRandTicker();
+      }
+      const url =
+        "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
+        { ticker } +
+        "&interval=1min" +
+        `&apikey=${process.env.STOCK_KEY}`;
+      axios.get(url).then(function (res) {
+        StockData = res;
+      });
+    }
+  }
+
+  useEffect(() => {
+    check();
+    getStockData();
+    setInterval(check, 15000 * 60);
+    setInterval(getStockData, 300000);
+  }, []);
   return (
-    <>
+    <div className={styles.mainContainer}>
       <div className={styles.mainContent}>
         <LoadingInfobar
-          StockName={"APPL"}
-          StartingPrice={155}
+          StockName={ticker}
+          StartingPrice={140}
           CurrentPrice={140}
         />
         <div className={styles.buttonOptions}>
@@ -22,6 +63,6 @@ export default function StockofTheDay() {
         </div>
       </div>
       <LeaderBoard />
-    </>
+    </div>
   );
 }
