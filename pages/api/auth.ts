9bbbb5d335a95e938b162b0be484017867d47b3d
Owen Na <102653237+owen-na@users.noreply.github.com>
2023-03-29 17:28:16 -0400

    Backend-Atuh (#50)
    * Changes to svg (#9)
    
    * Made header more responsive
    
    * fix darkmode
    
    * Further progress on positioning of loading bar and info
    
    * Moved home,globals style inside styles folder
    
    * Centered some main content
    
    * Made header more responsive
    
    * ARC FINALLY DISPLAYS ITSELF
    
    * fix darkmode
    
    * updated loadingbar postioning
    
    * Further progress on positioning of loading bar and info
    
    * Restructured the locations of the components
    
    * Moved
    
    ---------
    
    Co-authored-by: Steven Tran <steventran@Stevens-MacBook-Pro.local>
    Co-authored-by: Owen Na <newseoul1302@gmail.com>
    
    * SVG fix
    
    * Testing merge issue
    
    * Fixed postioning of footer and SOTD divs
    
    * Loading Bar stays visible at all ratios
    
    * FIXED POSTIONING OF MAINCONTENT
    
    * Removed some uneeded lines
    
    * Added Props for SOTD, cleaned up innerContent
    
    * In Progress on real time loadingbar
    
    * Loading Bar fills statically
    
    * Figured out total minutes till next day
    
    * Rough static frontend implemented
    
    * calculating time difference for percent fill now works as intended
    
    * Removed some unneeded code
    
    * Changed some styling for mainpage
    
    * Changed some styling, began working on Acc info page
    
    * Added Copyright, Barebones for User Info
    
    * Added Placeholder for Leagues Page
    
    * Created Prop interface and func for guesses correct
    
    * Implemented CalcCorrect %
    
    * Updated for correct calc for account page
    
    * Imported image, created Register page
    
    * Fixed Issue regarding overloaded function
    
    * Created a better placeholder for pfp image for now
    
    * began implementation of new pfps
    
    * Got some sort of pfp color randomizer working
    
    * Created Barebones for login
    
    * completed Barebones for login
    
    * Begun working on leaderboardlayout
    
    * Created statically placed cards
    
    * Changed some styling for accountPage
    
    * extra styling done for SOTD page
    
    * Added reccomendatins
    
    * Implemented models with typeorm (incomp.)
    
    * editied with typeorm to access the db
    
    * begin working on Auth
    
    * began on auth and db for login
    
    * Auth is not working
    
    * Further Work on login auth with nextauth
    
    * more work done for backend
    
    * Updated readme
    
    * Finished backend routing and basic auth???
    
    ---------
    
    Co-authored-by: Steven Tran <steventran@Stevens-MacBook-Pro.local>
    Co-authored-by: Owen Na <newseoul1302@gmail.com>

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,12 +1,13 @@
 import NextAuth from 'next-auth';
 import bcrypt from 'bcryptjs';
-import AppDataSource from '../../sb-be/server';
-import { Account } from '../../sb-be/models/accountModel';
 import CredentialsProvider from "next-auth/providers/credentials";
-
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 
 // when deploying, use NEXTAUTH_URL=https://example.com 
 // When deploying your site set the NEXTAUTH_URL environment variable to the canonical URL of the website.
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
   providers: [
@@ -17,35 +18,31 @@ export default NextAuth({
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials, req ) { // any typing for temp fix, def needs change for better later on...
-        const { username, password } = credentials;
-
         // Find the user with the given username in the database
-        const userRepository = (await AppDataSource).getRepository(Account);
-        const user = await userRepository.findOne({ where: { username } });
+      const { username, password } = credentials || {};
+      if (!username || !password) {
+        return null;
+      }
 
-        if (!user) {
-          throw new Error('Invalid username or password');
-        }
+      const user = await prisma.user.findUnique({
+        where: { username },
+      });
 
-        // Compare the given password with the user's hashed password
-        const passwordMatches = await bcrypt.compare(password, user.password);
+      if (!user) {
+        throw new Error('Invalid username or password');
+      }
 
-        if (!passwordMatches) {
-          throw new Error('Invalid username or password');
-        }
+      // Compare the given password with the user's hashed password
+      const passwordMatches = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatches) {
+        throw new Error('Invalid username or password');
+      }
 
         // If the password matches, return the user object
-        return user;
+      return user;
       },
     }),
   ],
-
-  session: {
-    jwt: true,
-  },
-
-  jwt: {
-    secret: process.env.JWT_SECRET,
-  },
+  adapter: PrismaAdapter(prisma),
 });
-
