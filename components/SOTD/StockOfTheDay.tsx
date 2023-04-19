9b7d1e03d93498812299d68411f917f2115375a4
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-04-19 10:49:57 -0400

    lol

--- components/SOTD/StockOfTheDay.tsx
+++ components/SOTD/StockOfTheDay.tsx
@@ -5,15 +5,20 @@ import f500ticker from "@assets/f500tickers.json";
 import axios from "axios";
 import Random from "@assets/Random";
 import React, { useState, useEffect } from "react";
-
 // reccomended to put this in the back, since this will be unique for each user so to make sure not
 // every user gets a different stock on refresh. -Alex
 
 export default function StockofTheDay() {
+  useEffect(() => {
+    check();
+    getStockData();
+    setInterval(check, 15000 * 60);
+    setInterval(getStockData, 300000);
+  }, []);
   let date = new Date();
   let marketStatus = false;
-  let StockData;
-  let stockRefreshDate;
+  let StockData: any;
+  let stockRefreshDate: any;
   const tickerJSON = f500ticker;
   const [ticker, setTicker] = useState("");
 
@@ -34,28 +39,32 @@ export default function StockofTheDay() {
   };
 
   async function getStockData() {
-    if (marketStatus == true) {
-      if (ticker == "" || date.getUTCHours() == 14) {
-        setRandTicker();
-      }
-      console.log(ticker);
+    if (StockData == undefined) {
       const url =
         "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
         test +
         "&interval=1min" +
-        `&apikey=`; // ${process.env.STOCK_KEY}
-      axios.get(url).then(function (res) {
+        `&apikey=${process.env.REACT_APP_STOCK_KEY}`; // ${process.env.STOCK_KEY}
+      axios.get(url).then((res: any) => {
         StockData = res;
+      });
+    }
+    if (marketStatus == true) {
+      if (ticker == "" || date.getUTCHours() == 14) {
+        setRandTicker();
+      }
+      const url =
+        "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" +
+        test +
+        "&interval=1min" +
+        `&apikey=${process.env.REACT_APP_STOCK_KEY}`; // ${process.env.STOCK_KEY}
+      axios.get(url).then((res: any) => {
+        StockData = res;
+        stockRefreshDate = res["Meta Data"]["3. Last Refreshed"];
       });
     }
   }
 
-  useEffect(() => {
-    check();
-    getStockData();
-    setInterval(check, 15000 * 60);
-    setInterval(getStockData, 300000);
-  }, []);
   return (
     <div className={styles.mainContainer}>
       <div className={styles.mainContent}>
@@ -65,6 +74,7 @@ export default function StockofTheDay() {
         </div>
         <LoadingInfobar
           StockName={ticker}
+          // StartingPrice={StockData["Time Series (5min)"][stockRefreshDate]}
           StartingPrice={140}
           CurrentPrice={140}
         />
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    STOCK_KEY: process.env.STOCK_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
